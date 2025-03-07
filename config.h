535b887981cb468d26244d77b0da2d2d36d14386
Saurabh Datta <hi@dattasaurabh.com>
2025-03-07 19:20:31 +0100

    TO satrt esp eye implementation

--- config.h
+++ config.h
@@ -6,13 +6,26 @@
 // -----------------------------------------------------------------
 #define CAMERA_MODEL_XIAO_ESP32S3 1
 // #define CAMERA_MODEL_AI_THINKER 1
+// #define CAMERA_MODEL_ESP_EYE 1
 
 // Error checking for camera model selection
-#if defined(CAMERA_MODEL_XIAO_ESP32S3) && defined(CAMERA_MODEL_AI_THINKER)
+#if defined(CAMERA_MODEL_XIAO_ESP32S3) && defined(CAMERA_MODEL_AI_THINKER) && defined(CAMERA_MODEL_ESP_EYE)
 #error "Please select only one camera model by uncommenting its define"
 #endif
 
-#if !defined(CAMERA_MODEL_XIAO_ESP32S3) && !defined(CAMERA_MODEL_AI_THINKER)
+#if defined(CAMERA_MODEL_XIAO_ESP32S3) && defined(CAMERA_MODEL_AI_THINKER) && !defined(CAMERA_MODEL_ESP_EYE)
+#error "Please select only one camera model by uncommenting its define"
+#endif
+
+#if defined(CAMERA_MODEL_XIAO_ESP32S3) && !defined(CAMERA_MODEL_AI_THINKER) && defined(CAMERA_MODEL_ESP_EYE)
+#error "Please select only one camera model by uncommenting its define"
+#endif
+
+#if !defined(CAMERA_MODEL_XIAO_ESP32S3) && defined(CAMERA_MODEL_AI_THINKER) && defined(CAMERA_MODEL_ESP_EYE)
+#error "Please select only one camera model by uncommenting its define"
+#endif
+
+#if !defined(CAMERA_MODEL_XIAO_ESP32S3) && !defined(CAMERA_MODEL_AI_THINKER) && !defined(CAMERA_MODEL_ESP_EYE)
 #error "Please select a camera model by uncommenting its define"
 #endif
 
